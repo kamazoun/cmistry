8a94b1ae98b29d117d5a5187dd6876ce4f445078
kamazoun <kamazoun@hotmail.fr>
2023-09-26 12:42:36 +0000

    Added main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+#define ELEMENTS 78
+
+int main()
+{
+    int numb;
+    numb = ELEMENTS;
+    printf("The number of elements is %d", numb);
+
+    printf('\n');
+    return 1;
+}
