c8e0d5a97460569213d12c264e2f9d84c578ab82
kamazoun <kamazoun@hotmail.fr>
2023-09-27 19:55:29 +0000

    silly things

--- main.c
+++ main.c
@@ -8,6 +8,17 @@ int main()
     numb = ELEMENTS;
     printf("The number of elements is %d", numb);
 
-    printf('\n');
+    char symbol;
+    while(symbol = getchar(), symbol != '1'){
+        printf("%c", symbol);
+    }
+
+    int x = 1;
+    int y = 0;
+    int t;
+    t = x, x = y, y = t;
+    printf("%d", x);
+
+    printf("\n");
     return 1;
 }
